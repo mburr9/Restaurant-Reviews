40f87d83821278c05ea5a7073383ea99a85cef29
Mark Burr <mburr9@gmail.com>
2018-11-14 10:44:47 -0500

    add service worker functionality

--- sw.js
+++ sw.js
@@ -1,1 +1,51 @@
+const cacheFiles = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/responsive.css',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg'
+];
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(cacheFiles);
+    })
+  )
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      const fetchEvent = event.request.clone();
+      return fetch(fetchEvent)
+      .then(function(response) {
+        const responseClone = response.clone();
+        caches.open('v1').then(function(cache) {
+          cache.put(event.request, responseClone);
+        })
+        return response;
+      })
+      .catch(function(err) {
+        console.log(err);
+      })
+    })
+  );
+});
